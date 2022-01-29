458697318e2ca7ce6bcd82f061bb3a48072ed1b1
UmuhireJessie <j.umuhire@alustudent.com>
2022-01-29 13:14:37 +0200

    q1

---
+++ question1.py
@@ -0,0 +1,7 @@
+"""State the Big O-notation and implement the algorithm that perform the following operations:
+
+The algorithm takes as input a list of numbers which may contain duplicates. It returns true if
+all elements of the list occur an odd number of times. Otherwise it returns false. For example, on the
+list {1, 3, 2, 2, 5, 2} it returns true, but on the list {1, 3, 2, 2, 5, 2, 5} it returns false because
+5 occurs an even number of times. [5] """
+
