b042ada3699be2d38c164bcb7bdb0f494e04923f
UmuhireJessie <j.umuhire@alustudent.com>
2022-01-30 14:16:45 +0200

    answer to question 1

---
+++ question2.py
@@ -0,0 +1,8 @@
+""" Question Two
+
+Design and implement an algorithm that takes: a list containing n distinct natural numbers and a number k ≤ n and
+calculates the sum of the k largest numbers in the array. For example, if the list is {3, 7, 5, 12, 6} and k = 3, then
+the algorithm should return 25= (12+7+6).
+
+Implement the algorithm in python [5] """
+
